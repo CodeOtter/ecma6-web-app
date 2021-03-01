402329e40f3ca42bfe2ce5eed71da4434d663363
Patrick Ryan <patrick.ryan@emblem21.com>
2021-03-01 16:00:59 +0000

    Mocking Twilio

--- test/mocks/providers/SmsProvider.js
+++ test/mocks/providers/SmsProvider.js
@@ -2,12 +2,12 @@ const getProvider = require('../../../src/providers/SmsProvider').default
 // const td = require('testdouble')
 
 const SmsProvider = (options) => {
-  const provider = getProvider(options)
+  // const provider = getProvider(options)
 
   // @SETUP: Add methods you need to mock for HttpService here
   // td.replace(provider, 'start')
 
-  return provider
+  return {}
 }
 
 module.exports = SmsProvider
